21646c2e4889a7cd009f3f6ad32fcf9d2711e02d
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-28 15:05:20 +0200

    NinGeneric Collections - ArrayLists

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,6 +3,18 @@ namespace Demo
 {
     internal class Program
     {
+        //public static int SumArrayList (ArrayList array)
+        //{
+        //    int sum = 0;
+        //    if(array != null)
+        //    {
+        //        for (int i = 0; i < array.Count; i++)
+        //        {
+        //            sum += (int)array[i];
+        //        }
+        //    }
+        //    return sum;
+        //}
         static void Main(string[] args)
         {
             #region NonGenaric Collections - ArrayLists
@@ -16,8 +28,19 @@ namespace Demo
             //arrayList.Add(5);
             //Console.WriteLine($"Count : {arrayList.Count} , Capacity : {arrayList.Capacity}");
 
-            ArrayList array = new ArrayList(10);
+            //ArrayList arrayList = new ArrayList(10) { 1,2,3,4,5,};
+            //arrayList.Add(6);
+            //arrayList.TrimToSize();// Delete UnUsed Bytes
+            //Console.WriteLine($"Count : {arrayList.Count} , Capacity : {arrayList.Capacity}");
+
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add(1);
+            //arrayList.Add(2);
+            //arrayList.Add(3);
+            //SumArrayList(arrayList);
             #endregion
+
+            
         }
     }
 }
