301e4f7f3d47c3df3f07b73c498bcf37809fb312
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-28 22:20:20 +0200

    Q01-GreaterThanQueries

---
+++ Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void CountGreaterNums(int[] arr, int[]queries)
+        {
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(arr is not null && queries is not null)
+                    {
+                        if (arr[j] > queries[i])
+                            count++;
+                    }                   
+                }
+                Console.WriteLine(count);
+            } 
+        }
+        static void Main(string[] args)
+        {
+            #region Q01-GreaterThanQueries
+            int[] arr = { 3, 5, 11 };
+            int[] queries = { 1, 5, 13 };
+           //CountGreaterNums(arr, queries); 
+            #endregion
+        }
+    }
+}
