3521431191b4c7ead2edf83c8301c84f8033722a
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-28 14:50:32 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region NonGenaric Collections - ArrayLists
+            //ArrayList arrayList = new ArrayList();
+
+            //Console.WriteLine($"Count : {arrayList.Count} , Capacity : {arrayList.Capacity}");
+            //arrayList.Add(1);
+            //arrayList.Add(2);
+            //arrayList.AddRange(new int[] { 3, 4 });
+            //Console.WriteLine($"Count : {arrayList.Count} , Capacity : {arrayList.Capacity}");
+            //arrayList.Add(5);
+            //Console.WriteLine($"Count : {arrayList.Count} , Capacity : {arrayList.Capacity}");
+
+            ArrayList array = new ArrayList(10);
+            #endregion
+        }
+    }
+}
