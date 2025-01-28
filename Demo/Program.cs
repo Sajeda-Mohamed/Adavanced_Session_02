18f5ec39ef92fcbaa641b1f187290fa139bb2ce3
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-28 17:00:34 +0200

    Lists Methods

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -41,26 +41,66 @@ namespace Demo
             #endregion
 
             #region Genaric Collections - Lists
-            List<int> Numbers = new List<int>(10) { 1, 2, 3, 4, 5 };
+            //List<int> Numbers = new List<int>(10) { 1, 2, 3, 4, 5 };
             //Console.WriteLine($"Count : {Numbers.Count} , Capacity : {Numbers.Capacity}");
 
             //Numbers.Add(1);
             //Numbers.Add(2);
             //Numbers.AddRange(new int[] {3,4});
 
-            Numbers.Add(6);
-            Numbers.TrimExcess(); //  DeAllocate
+            //Numbers.Add(6);
+            //Numbers.TrimExcess(); //  DeAllocate
             //foreach (int item in Numbers)
             //{
             //    Console.WriteLine(item);
             //}
             //Console.WriteLine($"Count : {Numbers.Count} , Capacity : {Numbers.Capacity}");
 
-            Numbers[0] = 100;  // Use Indexer as Setter
+            //Numbers[0] = 100;  // Use Indexer as Setter
             //Console.WriteLine(Numbers[0]);//Use Indexer as Getter
 
             //Console.WriteLine($"Sum is : {SumArrayList(Numbers)}");
             #endregion
+
+            #region Lists Methods
+            //List<int> Numbers = new List<int>(10) { 1, 2, 3, 4, 5 };
+            //Numbers.Insert(Numbers[0], 100);
+            //foreach (int i in Numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("======");
+            //Numbers.InsertRange(Numbers[3], new int[] { 7, 8, 9 });
+            //foreach (int i in Numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Numbers.Clear();
+            //Numbers.Contains(3);
+            //Numbers.EnsureCapacity(20);
+
+            //int[] Nums = new int[20];
+            //Numbers.CopyTo(Nums);
+            //foreach (int i in Nums)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Numbers.CopyTo(Nums, 4);
+            //foreach (int i in Nums)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Numbers.CopyTo(3, Nums, 4, 2);
+            //foreach (int i in Nums)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+
         }
     }
 }
